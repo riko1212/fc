c6d268b0928b62c39eba39f949dcd160a18b3ff4
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2024-06-02 21:24:07 +0300

    add restore pass logic

--- src/pages/RestorePass.jsx
+++ src/pages/RestorePass.jsx
@@ -1,36 +1,64 @@
-import { Link } from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
 
-function RestorePass () {
+function RestorePass() {
+  const navigate = useNavigate();
+
+  const handleRestorePass = (e) => {
+    e.preventDefault();
+    const name = e.target.elements['login-name'].value;
+    const newPassword = e.target.elements['new-pass'].value;
+    const repeatPassword = e.target.elements['repeat-pass'].value;
+
+    if (newPassword !== repeatPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    let users = JSON.parse(localStorage.getItem('users')) || [];
+    const userIndex = users.findIndex((user) => user.name === name);
+
+    if (userIndex === -1) {
+      alert('User not found');
+      return;
+    }
+
+    users[userIndex].password = newPassword;
+    localStorage.setItem('users', JSON.stringify(users));
+    alert('Password successfully updated');
+
+    navigate('/');
+  };
+
   return (
     <div className="login-wrapper">
       <div className="login-block">
         <p className="login-text">Restore Pass</p>
-        <form className="login-form">
-        <input
-          type="text"
-          name="login-name"
-          className="form-input"
-          placeholder="Enter name"
-        />
-        <input
-          type="password"
-          name="login-pass"
-          className="form-input"
-          placeholder="Enter new password"
-        />
-        <input
-          type="password"
-          name="login-pass"
-          className="form-input"
-          placeholder="Repeat new password"
-        />
-        {/* <button type="submit" className="btn form-btn ">Let me in</button> */}
-        <Link to="/" className="btn form-btn">Let me in</Link>
-
+        <form className="login-form" onSubmit={handleRestorePass}>
+          <input
+            type="text"
+            name="login-name"
+            className="form-input"
+            placeholder="Enter name"
+          />
+          <input
+            type="password"
+            name="new-pass"
+            className="form-input"
+            placeholder="Enter new password"
+          />
+          <input
+            type="password"
+            name="repeat-pass"
+            className="form-input"
+            placeholder="Repeat new password"
+          />
+          <button type="submit" className="btn form-btn">
+            Restore Password
+          </button>
         </form>
-      </div>  
+      </div>
     </div>
-  )
+  );
 }
 
-export default RestorePass
+export default RestorePass;
